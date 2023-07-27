02cb4b04ae385b5a054cfb701af356d3befa9564
Davranbek <davranbek_1995@mail.ru>
2023-07-28 00:20:45 +0500

    added delete, add post

--- src/js/profile.js
+++ src/js/profile.js
@@ -83,15 +83,75 @@ function listRender(state, selector) {
  
     if (state.length) {
       
-        state?.forEach(() => {
+        state?.forEach((el) => {
                 const card = document.createElement("div");
                 card.classList.add("card");
-                card.innerHTML = `<img src="https://picsum.photos/id/211/300/300"><h1>Lorem </h1>`;
+                card.innerHTML = `
+                    <div class="post_item relative shadow-md hover:shadow-xl duration-150 rounded-lp p-4">
+                    
+                        <div class="flex absolute right-3 gap-x-3">
+                            <button type="button" data-del="${el.id}" class="delete-post bg-red-500 w-8 h-8 rounded-lg">
+                                <i data-del="${el.id}" class="delete-post bx bx-trash text-xl text-white">
+                            </button>
+
+
+                            <button type="button" class="bg-blue-500 w-8 h-8 rounded-lg">
+                                <i class="bx bx-edit text-xl text-white">
+                            </button>
+                        </div> 
+
+
+                        <h2 class="post__title text-[28px] font-bold mb-5">${el.title}</h2>
+                        <p class="post__text mb-[25.5px] leading-[25.5px] text-[17px]">
+                            ${el.body?.substring(0, 256)} . . .
+                        </p>
+                        
+                        <strong class="mb-[10px] leading-[25.5px]">${localStorage.getItem("username")}</strong>
+
+                        <div class="flex items-center gap-x-3 my-4 text-[#949494] text-[14px] leading-[19.6px]">
+                            <span>${el.createdAt.substring(0, 10)}</span>
+                            <i class='bx bx-show'></i>
+                            <span>${el.views}</span>
+                        </div>
+                    </div>
+                
+                `;
                 $("#" + selector).append(card);
         })
     } else {
         $('#'+selector).innerHTML = `<h1 class='text-center'>${selector.toUpperCase()} NOT FOUND</h1>`;
    }
 }
+
+
+
+/////////////////// delete posts //////////////////////////////////////////////
+
+function deletePosts(id) {
+    console.log(id);
+    if (id) {
+        fetch(`https://nest-blog-qdsa.onrender.com/api/blog/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        })
+        .then((res) => res.json())
+        .then(() => {
+            alert("Post item deleted successfully");
+            window.location.reload();
+        });
+    }
+}
+
+$("#posts").addEventListener("click", (e) => {
+    console.log("on clicked");
+    if (e.target.classList.contains("delete-post")) {
+        let uniqueID = e.target.getAttribute("data-del");
+        deletePosts(uniqueID);
+    }
+});
+
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,7 @@ $$('.change-btn').forEach((btn, index) => {
     
 })
 
+//////////// tab activation actions  ////////////////
 
 function hideContent(){
     $$('.tab-item').forEach(tab => {
@@ -47,6 +48,11 @@ $('#closeModal').addEventListener('click', () => {
     $('.modal-wrapper').classList.remove('grid');
 })
 
+
+$('#add_btn').addEventListener('click', () => {
+    $('.modal-wrapper').classList.add('grid');
+    $('.modal-wrapper').classList.remove('hidden');
+})
 
 
 
@@ -129,10 +135,16 @@ function authCeck(){
         $(".menu").classList.remove("hidden");
         $("#openModal").classList.add("hidden");
         $('#user_info').textContent = localStorage.getItem('username');
+        
+        $("#auth").classList.add("hidden");
+        $("#addpost").classList.remove("hidden");
     }else {
         $(".menu").classList.add("hidden");
         $(".dropdown").classList.add("hidden");
         $("#openModal").classList.remove("hidden");
+
+        $("#auth").classList.remove("hidden");
+        $("#addpost").classList.add("hidden");
     }
 }
 
@@ -146,4 +158,37 @@ $('.menu').addEventListener('click', () => {
 $('#logout').addEventListener('click', () => {
     localStorage.clear();
     location.reload();
+})
+
+
+////////////////////// Blog post ///////////////
+
+function addPost(){
+    const newBlog = {
+        title: $("#blog_title").value,
+        body: $("#blog_text").value,
+        user_id: localStorage.getItem("user")
+    }
+
+   
+    if(newBlog.title.trim().length === 0 || newBlog.body.trim().length === 0){
+        alert("Please fill title and body!");
+    }else{
+        fetch(`https://nest-blog-qdsa.onrender.com/api/blog`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify(newBlog),
+        })
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err.message))
+    }
+}
+
+
+$('#save').addEventListener('click', () => {
+    addPost();
 })
